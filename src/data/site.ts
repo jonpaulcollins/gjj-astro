4b9fec6947ba0ba8a2d671a60af2c882feea778e
jonpaulcollins <jonpaulcollins@gmail.com>
2026-07-28 14:01:12 +0100

    Create site.ts

---
+++ src/data/site.ts
@@ -0,0 +1,26 @@
+export const site = {
+  name: "Guildford Jiu Jitsu",
+  domain: "https://guildfordbjj.com",
+  phone: "+447575572021",
+  phoneDisplay: "+44 7575 572021",
+  email: "info@guildfordbjj.com",
+  address: {
+    street: "1-2 Bridge Street",
+    locality: "Guildford",
+    region: "Surrey",
+    postalCode: "GU1 4RY",
+    country: "UK",
+  },
+  social: {
+    facebook: "https://facebook.com/GuildfordBJJ",
+    instagram: "https://instagram.com/guildford.bjj",
+    youtube: "https://youtube.com/@GuildfordBJJ",
+    tiktok: "https://tiktok.com/@guildford.bjj",
+  },
+  // TODO: replace with real GHL embed snippets before launch
+  ghl: {
+    formEmbed: "<!-- Paste GHL free-trial form embed code here -->",
+    calendarEmbed: "<!-- Paste GHL calendar/booking widget embed code here -->",
+    whatsappNumber: "447575572021",
+  },
+};
