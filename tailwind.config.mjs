3eab07feee20a3c09d93f2dafd22a4bf56dc025e
jonpaulcollins <jonpaulcollins@gmail.com>
2026-07-28 14:10:32 +0100

    Create tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          red: '#E11D2A',
+          black: '#0a0a0a',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
