39302e94d65edf9fa8ed4b6b70a5dbaaa7d3884f
jonpaulcollins <jonpaulcollins@gmail.com>
2026-08-11 12:43:26 +0100

    Canonical domain to www, add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://guildfordbjj.com',
-  integrations: [tailwind()],
+  site: 'https://www.guildfordbjj.com',
+  integrations: [tailwind(), sitemap()],
 });
